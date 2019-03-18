fcf06d8c169190ae0e86ac24aa721f71f46bc1b3
aclarke9691 <aclarke9691@bths.edu>
2019-03-18 14:40:12 -0400

    Love turtles

--- classes/IntArrayWorker.java
+++ classes/IntArrayWorker.java
@@ -115,8 +115,8 @@ public class IntArrayWorker
   {
       int bigger = 0;
       for (int i = 0; i<matrix.length;i++){
-          for(int j=0;j<matrix.length-1;j++){
-              if(matrix[i][j] > matrix[(i+1)][(j+1)]){
+          for(int j=0;j<matrix[0].length;j++){
+              if(matrix[i][j] > bigger){
                   bigger = matrix[i][j];
               }
           }
