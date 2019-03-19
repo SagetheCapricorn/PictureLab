2cf9a4d0d10b350e26bfbaa78ffb35d298d8dcec
OENFGNIO <SDFIOB>
2019-03-19 14:41:04 -0400

    sage

--- classes/PictureTester.java
+++ classes/PictureTester.java
@@ -50,7 +50,13 @@ public class PictureTester
     swan.edgeDetection(10);
     swan.explore();
   }
-  
+
+
+  public static void testNegate(){
+    Picture canvas = new Picture("beach.jpg");
+    canvas.negate();
+    canvas.explore();
+  }
   /** Main method for testing.  Every class can have a main
     * method in Java */
   public static void main(String[] args)
@@ -58,11 +64,11 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
+    //testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
-    //testNegate();
+    testNegate();
     //testGrayscale();
     //testFixUnderwater();
     //testMirrorVertical();
